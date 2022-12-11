d3e360c57a394c099ea6c02d071f090dc7f89d9d
Nat1an <nathan@nat1an.xyz>
2022-12-11 12:25:44 +0800

    Implement most of play command

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,14 +1,18 @@
 import re
+from asyncio import sleep
 
-from disnake import Option, ApplicationCommandInteraction, Embed, Color
+from disnake import Option, ApplicationCommandInteraction, OptionType
 from disnake.ext import commands
 from disnake.ext.commands import Cog
+from lavalink import DefaultPlayer, LoadResult, LoadType
 
-from library.functions import ensure_voice
+from core.classes import Bot
+from core.embeds import ErrorEmbed, SuccessEmbed
+from library.functions import ensure_voice, update_display
 
 
 class Commands(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.slash_command(
@@ -18,58 +22,65 @@ class Commands(Cog):
             Option(
                 name="query",
                 description="歌曲名稱或網址，支援 YouTube, YouTube Music, SoundCloud, Spotify",
+                type=OptionType.string,
+                required=True
+            ),
+            Option(
+                name="index",
+                description="要將歌曲放置於當前播放序列的位置",
+                type=OptionType.integer,
+                required=False
             )
         ]
     )
-    async def play(self, interaction: ApplicationCommandInteraction, query: str):
-        await ensure_voice(self.bot, interaction.author, interaction.channel, should_connect=True)
+    async def play(self, interaction: ApplicationCommandInteraction, query: str, index: int = None):
+        await interaction.response.defer()
 
-        player = self.bot.lavalink.player_manager.get(interaction.guild.id)
+        await ensure_voice(interaction, should_connect=True)
+
+        player: DefaultPlayer = self.bot.lavalink.player_manager.get(interaction.guild.id)
+
+        player.store("channel", interaction.channel.id)
 
         url_rx = re.compile(r"https?://(?:www\.)?.+")
 
-        # Check if the user input might be a URL. If it isn't, we can Lavalink do a YouTube search for it instead.
-        # SoundCloud searching is possible by prefixing "scsearch:" instead.
         if not url_rx.match(query):
             query = f'ytsearch:{query}'
 
         # Get the results for the query from Lavalink.
-        results = await player.node.get_tracks(query)
+        results: LoadResult = await player.node.get_tracks(query)
 
-        # Results could be None if Lavalink returns an invalid response (non-JSON/non-200 (OK)).
-        # Alternatively, results.tracks could be an empty array if the query yielded no tracks.
         if not results or not results.tracks:
-            return await interaction.response.send_message('Nothing found!', ephemeral=True)
+            return await interaction.edit_original_response(embed=ErrorEmbed("沒有找到任何歌曲"))
 
-        embed = Embed(color=Color.blurple())
+        match results.load_type:
+            case LoadType.TRACK:
+                player.add(requester=interaction.author.id, track=results.tracks[0], index=index)
 
-        # Valid loadTypes are:
-        #   TRACK_LOADED    - single video/direct URL)
-        #   PLAYLIST_LOADED - direct URL to playlist)
-        #   SEARCH_RESULT   - query prefixed with either ytsearch: or scsearch:.
-        #   NO_MATCHES      - query yielded no results
-        #   LOAD_FAILED     - most likely, the video encountered an exception during loading.
-        if results.load_type == 'PLAYLIST_LOADED':
-            tracks = results.tracks
+                await interaction.edit_original_response(
+                    embed=SuccessEmbed(f"已加入播放序列", f"{results.tracks[0].title}")
+                )
 
-            for track in tracks:
-                # Add all of the tracks from the playlist to the queue.
-                player.add(requester=interaction.author.id, track=track)
+            case LoadType.PLAYLIST:
+                # TODO: Ask user if they want to add the whole playlist or just some tracks
 
-            embed.title = 'Playlist Enqueued!'
-            embed.description = f'{results.playlist_info.name} - {len(tracks)} tracks'
-        else:
-            track = results.tracks[0]
-            embed.title = 'Track Enqueued'
-            embed.description = f'[{track.title}]({track.uri})'
+                for iter_index, track in enumerate(results.tracks):
+                    player.add(requester=interaction.author.id, track=track, index=index + iter_index)
 
-            player.add(requester=interaction.author.id, track=track)
-
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+                await interaction.edit_original_response(
+                    embed=SuccessEmbed(
+                        f"已將 {results.playlist_info.name} 中的 {len(results.tracks)} 首歌曲加入播放序列",
+                        '\n'.join([f"{index + 1}. {track.title}" for index, track in enumerate(results.tracks)])
+                    )
+                )
 
         # If the player isn't already playing, start it.
         if not player.is_playing:
             await player.play()
+
+        await sleep(5)
+
+        await update_display(self.bot, player, await interaction.original_response())
 
 
 def setup(bot):
