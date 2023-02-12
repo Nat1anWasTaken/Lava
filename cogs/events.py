4ef3650b2f555047b0d321e214b0a003e72abdbe
Nat1an <nathan@nat1an.xyz>
2023-02-12 11:49:25 +0800

    fix: Formats in cogs/events.py

--- cogs/events.py
+++ cogs/events.py
@@ -64,102 +64,101 @@ class Events(Cog):
             channel: Union[GuildChannel, TextChannel, Thread] = self.bot.get_channel(int(player.fetch("channel")))
 
             message = await channel.send(
-                embed=ErrorEmbed(f"無法播放歌曲: {event.track['title']}", f"原因: {event.original}")
+                embed=ErrorEmbed(f"無法播放歌曲: {event.track['title']}", f"原因 {event.original}")
             )
 
             await player.skip()
 
-            await update_display(self.bot, player, message, delay=5)
+            await update_display(self.bot, player, message)
 
-            @ commands.Cog.listener(name="on_slash_command_error")
-            async
+    @commands.Cog.listener(name="on_slash_command_error")
+    async def on_slash_command_error(self, interaction: ApplicationCommandInteraction, error: CommandInvokeError):
+        if isinstance(error.original, MissingVoicePermissions):
+            embed = ErrorEmbed("指令錯誤", "我需要 `連接` 和 `說話` 權限才能夠播放音樂")
 
-            def on_slash_command_error(self, interaction: ApplicationCommandInteraction, error: CommandInvokeError):
-                if isinstance(error.original, MissingVoicePermissions):
-                    embed = ErrorEmbed("指令錯誤", "我需要 `連接` 和 `說話` 權限才能夠播放音樂")
+        elif isinstance(error.original, BotNotInVoice):
+            embed = ErrorEmbed("指令錯誤", "我沒有連接到一個語音頻道")
 
-                elif isinstance(error.original, BotNotInVoice):
-                    embed = ErrorEmbed("指令錯誤", "我沒有連接到一個語音頻道")
+        elif isinstance(error.original, UserNotInVoice):
+            embed = ErrorEmbed("指令錯誤", "你沒有連接到一個語音頻道")
 
-                elif isinstance(error.original, UserNotInVoice):
-                    embed = ErrorEmbed("指令錯誤", "你沒有連接到一個語音頻道")
+        elif isinstance(error.original, UserInDifferentChannel):
+            embed = ErrorEmbed("指令錯誤", f"你必須與我在同一個語音頻道 <#{error.original.voice.id}>")
 
-                elif isinstance(error.original, UserInDifferentChannel):
-                    embed = ErrorEmbed("指令錯誤", f"你必須與我在同一個語音頻道 <#{error.original.voice.id}>")
+        else:
+            raise error.original
 
-                else:
-                    raise error.original
+        try:
+            await interaction.response.send_message(embed=embed)
+        except InteractionResponded:
+            await interaction.edit_original_response(embed=embed)
 
-                try:
-                    await interaction.response.send_message(embed=embed)
-                except InteractionResponded:
-                    await interaction.edit_original_response(embed=embed)
+    @commands.Cog.listener(name="on_voice_state_update")
+    async def on_voice_state_update(self, member, before, after):
+        if (
+                before.channel is not None
+                and after.channel is None
+                and member.id == self.bot.user.id
+        ):
+            player: DefaultPlayer = self.bot.lavalink.player_manager.get(member.guild.id)
 
-        @commands.Cog.listener(name="on_voice_state_update")
-        async def on_voice_state_update(self, member, before, after):
-            if (
-                    before.channel is not None
-                    and after.channel is None
-                    and member.id == self.bot.user.id
-            ):
-                player: DefaultPlayer = self.bot.lavalink.player_manager.get(member.guild.id)
+            await player.stop()
+            player.queue.clear()
 
-                await player.stop()
-                player.queue.clear()
+            try:
+                await update_display(self.bot, player)
+            except ValueError:  # There's no message to update
+                pass
 
-                try:
-                    await update_display(self.bot, player)
-                except ValueError:  # There's no message to update
-                    pass
+    @commands.Cog.listener(name="on_message_interaction")
+    async def on_message_interaction(self, interaction: MessageInteraction):
+        if interaction.data.custom_id.startswith("control"):
+            if interaction.data.custom_id.startswith("control.empty"):
+                await interaction.response.edit_message()
 
-        @commands.Cog.listener(name="on_message_interaction")
-        async def on_message_interaction(self, interaction: MessageInteraction):
-            if interaction.data.custom_id.startswith("control"):
-                if interaction.data.custom_id.startswith("control.empty"):
-                    await interaction.response.edit_message()
+                return
 
-                    return
+            try:
+                await ensure_voice(interaction, should_connect=False)
+            except (UserNotInVoice, BotNotInVoice, MissingVoicePermissions, UserInDifferentChannel):
+                return
 
-                try:
-                    await ensure_voice(interaction, should_connect=False)
-                except (UserNotInVoice, BotNotInVoice, MissingVoicePermissions, UserInDifferentChannel):
-                    return
+            player: DefaultPlayer = self.bot.lavalink.player_manager.get(interaction.guild_id)
 
-                player: DefaultPlayer = self.bot.lavalink.player_manager.get(interaction.guild_id)
+            match interaction.data.custom_id:
+                case "control.resume":
+                    await player.set_pause(False)
 
-                match interaction.data.custom_id:
-                    case "control.resume":
-                        await player.set_pause(False)
+                case "control.pause":
+                    await player.set_pause(True)
 
-                    case "control.pause":
-                        await player.set_pause(True)
+                case "control.stop":
+                    await player.stop()
+                    player.queue.clear()
 
-                    case "control.stop":
-                        await player.stop()
-                        player.queue.clear()
+                case "control.previous":
+                    await player.seek(0)
 
-                    case "control.previous":
-                        await player.seek(0)
+                case "control.next":
+                    await player.skip()
 
-                    case "control.next":
-                        await player.skip()
+                case "control.shuffle":
+                    player.set_shuffle(not player.shuffle)
 
-                    case "control.shuffle":
-                        player.set_shuffle(not player.shuffle)
+                case "control.repeat":
+                    player.set_loop(player.loop + 1 if player.loop < 2 else 0)
 
-                    case "control.repeat":
-                        player.set_loop(player.loop + 1 if player.loop < 2 else 0)
+                case "control.rewind":
+                    await player.seek(round(player.position) - 10000)
 
-                    case "control.rewind":
-                        await player.seek(round(player.position) - 10000)
+                case "control.forward":
+                    await player.seek(round(player.position) + 10000)
 
-                    case "control.forward":
-                        await player.seek(round(player.position) + 10000)
+                case "control.autoplay":
+                    toggle_autoplay(player)
 
-                    case "control.autoplay":
-                        toggle_autoplay(player)
+            await update_display(self.bot, player, interaction=interaction)
 
-                await update_display(self.bot, player, interaction=interaction)
 
-    def setup(bot):
-        bot.add_cog(Events(bot))
+def setup(bot):
+    bot.add_cog(Events(bot))
