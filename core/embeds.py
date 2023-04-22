d46e1d8736b0813f83c2d98683a557e953ab953b
Nat1an <nathan@nat1an.xyz>
2023-04-22 22:54:02 +0800

    🔨 refactor(embeds.py): remove redundant string casting and add support for Localized strings

--- core/embeds.py
+++ core/embeds.py
@@ -5,24 +5,54 @@ from disnake import Embed, Localized
 
 class SuccessEmbed(Embed):
     def __init__(self, title: Union[str, Localized], description: Union[str, Localized] = None, **kwargs):
-        super().__init__(title="✅ | " + str(title), description=str(description), color=0x0f9d58, **kwargs)
+        if isinstance(title, Localized):
+            title = title.string
+
+        if isinstance(description, Localized):
+            description = description.string
+
+        super().__init__(title="✅ | " + title, description=description, color=0x0f9d58, **kwargs)
 
 
 class InfoEmbed(Embed):
     def __init__(self, title: Union[str, Localized], description: Union[str, Localized] = None, **kwargs):
-        super().__init__(title="ℹ️ | " + str(title), description=str(description), color=0x4285f4, **kwargs)
+        if isinstance(title, Localized):
+            title = title.string
+
+        if isinstance(description, Localized):
+            description = description.string
+
+        super().__init__(title="ℹ️ | " + title, description=description, color=0x4285f4, **kwargs)
 
 
 class LoadingEmbed(Embed):
     def __init__(self, title: Union[str, Localized], description: Union[str, Localized] = None, **kwargs):
-        super().__init__(title="⌛ | " + str(title), description=str(description), color=0x4285F4, **kwargs)
+        if isinstance(title, Localized):
+            title = title.string
+
+        if isinstance(description, Localized):
+            description = description.string
+
+        super().__init__(title="⌛ | " + title, description=description, color=0x4285F4, **kwargs)
 
 
 class WarningEmbed(Embed):
     def __init__(self, title: Union[str, Localized], description: Union[str, Localized] = None, **kwargs):
-        super().__init__(title="⚠ | " + str(title), description=str(description), color=0xf4b400, **kwargs)
+        if isinstance(title, Localized):
+            title = title.string
+
+        if isinstance(description, Localized):
+            description = description.string
+
+        super().__init__(title="⚠ | " + title, description=description, color=0xf4b400, **kwargs)
 
 
 class ErrorEmbed(Embed):
     def __init__(self, title: Union[str, Localized], description: Union[str, Localized] = None, **kwargs):
-        super().__init__(title="❌ | " + str(title), description=str(description), color=0xdb4437, **kwargs)
+        if isinstance(title, Localized):
+            title = title.string
+
+        if isinstance(description, Localized):
+            description = description.string
+
+        super().__init__(title="❌ | " + title, description=description, color=0xdb4437, **kwargs)
