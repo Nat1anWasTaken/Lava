759403d3255e78506db7af48cd7ffa6565ec8b8a
Nat1an <nathan@nat1an.xyz>
2023-04-27 20:14:59 +0800

    🎨 style(commands.py): format the code

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,7 +1,8 @@
 import re
 from os import getpid
 
-from disnake import Option, ApplicationCommandInteraction, OptionType, OptionChoice, ButtonStyle, Localized, Embed, SelectOption, MessageInteraction
+from disnake import Option, ApplicationCommandInteraction, OptionType, OptionChoice, ButtonStyle, Localized, Embed, \
+    SelectOption, MessageInteraction
 from disnake.ext import commands
 from disnake.ext.commands import Cog
 from disnake.ui import Button, StringSelect
@@ -160,7 +161,7 @@ class Commands(Cog):
                     )
                 )
             )
-        
+
         # Find the index song should be (In front of any autoplay songs)
         if not index:
             index = sum(1 for t in player.queue if t.requester)
@@ -241,16 +242,19 @@ class Commands(Cog):
                     )
                 ]
                 await interaction.edit_original_response(
-                embed = WarningEmbed(
-                    title=f"{self.bot.get_text('command.play.select_warning.title', locale, '您似乎沒有選到任何一首歌曲')}"
-                    ,description=f"{self.bot.get_text('command.play.select_warning.description', locale, '不過不要緊，你還可以透過底下的選單來選擇一首音樂')}"
+                    embed=WarningEmbed(
+                        title=f"{self.bot.get_text('command.play.select_warning.title', locale, '您似乎沒有選到任何一首歌曲')}"
+                        ,
+                        description=f"{self.bot.get_text('command.play.select_warning.description', locale, '不過不要緊，你還可以透過底下的選單來選擇一首音樂')}"
                     ),
-                components=components)
+                    components=components
+                )
 
                 select_interaction: MessageInteraction = await self.bot.wait_for(
                     "dropdown",
                     timeout=None,
-                    check=lambda i: i.author == interaction.author and i.data.custom_id == f"track_selection_{interaction.id}"
+                    check=lambda
+                        i: i.author == interaction.author and i.data.custom_id == f"track_selection_{interaction.id}"
                 )
 
                 re_results = await player.node.get_tracks(f'{select_interaction.data.values[0]}', check_local=True)
@@ -261,15 +265,16 @@ class Commands(Cog):
                     track=re_results.tracks[0], index=index
                 )
 
-                    # noinspection PyTypeChecker
+                # noinspection PyTypeChecker
                 await interaction.edit_original_response(
                     embeds=[
-                                SuccessEmbed(
-                                    self.bot.get_text("command.play.loaded.title", locale, "已加入播放序列"),
-                                    {re_results.tracks[0].title}
-                                )
-                            ] + filter_warnings,
-                components=None)
+                               SuccessEmbed(
+                                   self.bot.get_text("command.play.loaded.title", locale, "已加入播放序列"),
+                                   {re_results.tracks[0].title}
+                               )
+                           ] + filter_warnings,
+                    components=None
+                )
 
         # If the player isn't already playing, start it.
         if not player.is_playing:
