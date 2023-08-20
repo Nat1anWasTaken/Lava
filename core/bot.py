8fa16f872e848b89bba3701fc9cb788e47f9ea50
Nat1an <nathan@nat1an.xyz>
2023-08-20 15:14:03 +0800

    🔄 refactor(bot.py): move SourceManager class from core.sources.source to core.source module for better organization and naming consistency

--- core/bot.py
+++ core/bot.py
@@ -5,7 +5,7 @@ from disnake.abc import MISSING
 from disnake.ext.commands import Bot as OriginalBot
 from lavalink import Client
 
-from core.sources.source import SourceManager
+from core.source import SourceManager
 
 
 class Bot(OriginalBot):
