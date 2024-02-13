f31f7c3840ee2082ec62ae74f5bd87c49b7d27f8
Mantou <opcantel@gmail.com>
2024-02-13 22:44:08 +0800

    feat(main.py): Add set_activity function to set bot's activity.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from os import getenv
 
 from colorlog import ColoredFormatter
-from disnake import Intents
+from disnake import Intents, Streaming, Activity, Game, ActivityType
 from disnake.ext.commands import CommandSyncFlags
 from dotenv import load_dotenv
 
@@ -21,10 +21,15 @@ def main():
 
     loop = asyncio.new_event_loop()
 
+    activity = set_activity()    
+
     bot = Bot(
         logger=main_logger,
-        command_prefix=getenv("PREFIX", "l!"), intents=Intents.all(), loop=loop,
-        command_sync_flags=CommandSyncFlags.default()
+        command_prefix=getenv("PREFIX", "l!"),
+        intents=Intents.all(),
+        loop=loop,
+        activity=activity,
+        command_sync_flags=CommandSyncFlags.default(),
     )
 
     bot.i18n.load("locale/")
@@ -34,21 +39,41 @@ def main():
     bot.run(os.environ["TOKEN"])
 
 
+def set_activity():
+    with open("configs/activity.json", "r") as f:
+        activity = json.load(f)
+
+    status = activity["status"]
+    name = activity["name"]
+
+    match status:
+        case "playing":
+            activity = Game(name=name)
+        case "listening":
+            activity = Activity(type=ActivityType.listening, name=name)
+        case "streaming":
+            activity = Streaming(name=name, url=activity["url"])
+        case "watching":
+            activity = Activity(type=ActivityType.watching, name=name)
+
+    return activity
+
+
 def setup_logging():
     """
     Set up the loggings for the bot
     :return: None
     """
     formatter = ColoredFormatter(
-        '%(asctime)s %(log_color)s [%(levelname)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
+        "%(asctime)s %(log_color)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
         log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'white',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'bold_red',
-        }
+            "DEBUG": "cyan",
+            "INFO": "white",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
     )
 
     stream_handler = logging.StreamHandler()
@@ -59,9 +84,7 @@ def setup_logging():
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
-    logging.basicConfig(
-        handlers=[stream_handler, file_handler], level=logging.INFO
-    )
+    logging.basicConfig(handlers=[stream_handler, file_handler], level=logging.INFO)
 
 
 def load_extensions(bot: Bot) -> Bot:
