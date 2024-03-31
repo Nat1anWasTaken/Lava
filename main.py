d91790fe3fd8e5a87ff2684a8eb1fb6a3e4834c6
Mantou <opcantel@gmail.com>
2024-03-31 14:42:33 +0800

    refactor: Replace double quotes to single quotes.

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ def setup_logging():
     :return: None
     """
     formatter = ColoredFormatter(
-        "%(asctime)s %(log_color)s [%(levelname)s] %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        '%(asctime)s %(log_color)s [%(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
         log_colors={
-            "DEBUG": "cyan",
-            "INFO": "white",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        },
+            'DEBUG': 'cyan',
+            'INFO': 'white',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        }
     )
 
     stream_handler = logging.StreamHandler()
@@ -84,7 +84,9 @@ def setup_logging():
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
-    logging.basicConfig(handlers=[stream_handler, file_handler], level=logging.INFO)
+    logging.basicConfig(
+        handlers=[stream_handler, file_handler], level=logging.INFO
+    )
 
 
 def load_extensions(bot: Bot) -> Bot:
