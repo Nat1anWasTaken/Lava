7a198fdf6ac1b8edb79f45cb007d1cf9cbdd31e4
Nat1anWasTaken <nathan@nat1an.xyz>
2024-05-12 22:54:59 +0800

    fix(lava/cogs/commands.py, lava/cogs/events.py): remove redundant event dispatch calls

--- lava/cogs/events.py
+++ lava/cogs/events.py
@@ -136,7 +136,6 @@ class Events(Cog):
             match interaction.data.custom_id:
                 case "control.resume":
                     await player.set_pause(False)
-                    self.bot.dispatch("play_or_resume", player=player)
 
                 case "control.pause":
                     await player.set_pause(True)
